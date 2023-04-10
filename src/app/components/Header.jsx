538d7d1c3958a838a1f8af095fb5f4e62c9bc55e
Dmengine <azeezoladimeji23@gmail.com>
2023-04-10 14:52:32 +0100

    headersec

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 
 export const Header = () => {
   return (
-    <div>
+    <div className='flex justify-between mx-2 max-w-6xl sm:mx-auto items-center py-6'>
         <div className='flex'>
             <Menuitem title='Home' address="/" Icon={AiFillHome} />
             <Menuitem title='About' address="/about" Icon={BsInfoCircleFill} />
@@ -14,8 +14,8 @@ export const Header = () => {
         <div>
             <Link href="/">
                 <h2 className='text-2xl '>
-                    <span>IMDb</span>
-                    <span>Clone</span>
+                    <span className='font-bold bg-amber-500 py-2 px-2 rounded-lg mr-1'>IMDb</span>
+                    <span className='text-xl hidden sm:inline'>Clone</span>
                 </h2>
             </Link>
         </div>
--- src/app/components/Menuitem.jsx
+++ src/app/components/Menuitem.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export const Menuitem = ({title, address, Icon}) => {
   return (
     <div>
-        <Link href={address} className='mx-4 lg:mx-6 hover:text-amber-600' >
+        <Link href={address} className='mx-4 lg:mx-6 hover:text-amber-600 ' >
              <Icon className='text-2xl sm:hidden' />
             <p className='hidden sm:inline my-2 text-sm '>{title}</p>
         </Link>
