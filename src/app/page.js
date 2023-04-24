6cbead7948fedbaf58592f2c1d8358944a46cdc9
Dmengine <azeezoladimeji23@gmail.com>
2023-04-24 10:53:46 +0100

    API fetching

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ const API_Key = process.env.API_Key;
 
 export default async function Home({searchParams}) {
   const genre = searchParams.genre || "fetchTrending";
-  const res = await fetch(`https://api.themoviedb.org/3/${genre === "topRated" ? "movie/top_rated" : "trending/all/week" }?api_key${API_Key}&language=en-US&page=1`, { next: { revalidate: 10000 } })
+  const res = await fetch(`https://api.themoviedb.org/3/${genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week" }?api_key=${API_Key}&language=en-US&page=1`, { next: { revalidate: 10000 } })
 
   if(!res.ok) {
     throw new Error('failed to fetch data')
